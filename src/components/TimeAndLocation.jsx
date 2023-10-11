9cb2ca7a0426ede823933295393ac8a2e52331d1
Tom <tom.herbron@proton.me>
2023-10-11 12:07:58 +0200

    Installed unicons, build UI for cities, inputs, local date

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TimeAndLocation() {
+    return (
+        <div>
+            <div className="flex items-center justify-center my-6">
+                <p className="text-white text-xl font-extralight">Tuesday, 31 May 2022 | Local time: 12:46 PM</p>
+            </div>
+        </div>
+    )
+}
+
+export default TimeAndLocation
